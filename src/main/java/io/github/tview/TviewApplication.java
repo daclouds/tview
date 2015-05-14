5cdeb50d08f7caafbca15fff9f82a1d24a94cf43
daclouds <daclouds@gmail.com>
2015-05-14 15:36:21 +0900

    initial commit

---
+++ src/main/java/io/github/tview/TviewApplication.java
@@ -0,0 +1,12 @@
+package io.github.tview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TviewApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/tview/TviewApplicationTests.java
@@ -0,0 +1,20 @@
+package io.github.tview;
+
+import io.github.tview.TviewApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TviewApplication.class)
+@WebAppConfiguration
+public class TviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
